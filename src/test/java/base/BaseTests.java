9aead7f583ce4c36f441dbec988927ebb76ab6f8
wilson <wilsongumba@gmail.com>
2020-06-12 23:16:06 +0300

    Add login test

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -5,47 +5,87 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import pages.HomePage;
 
 import java.util.List;
 
 public class BaseTests {
 
     private WebDriver driver;
+    protected HomePage homePage;
+
+    @BeforeClass
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
+        driver.manage().window().maximize();
 
-        //maximize window
-        //driver.manage().window().maximize();
+        homePage = new HomePage(driver);
+    }
 
-        //full screen mode
-        //driver.manage().window().fullscreen();
-
-        //set specific size
-        //driver.manage().window().setSize(new Dimension(375, 812));
-
-
-        //find several elements and count
-        List<WebElement> links = driver.findElements(By.tagName("a"));
-        System.out.println(links.size());
-
-        //find specific element by linktext
-        //driver.findElement(By.linkText("Inputs"));
-        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
-        inputsLink.click();
-
-
-
-
-
-        System.out.println(driver.getTitle());
-
+    @AfterClass
+    public void tearDown(){
         driver.quit();
     }
-
-    public static void main(String args[]){
-        BaseTests test = new BaseTests();
-        test.setUp();
-    }
 }
+
+
+
+
+
+
+////without page object model (unencouraged)
+//package base;
+//
+//        import org.openqa.selenium.By;
+//        import org.openqa.selenium.Dimension;
+//        import org.openqa.selenium.WebDriver;
+//        import org.openqa.selenium.WebElement;
+//        import org.openqa.selenium.chrome.ChromeDriver;
+//
+//        import java.util.List;
+//
+//public class BaseTests {
+//
+//    private WebDriver driver;
+//    public void setUp(){
+//        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+//        driver = new ChromeDriver();
+//        driver.get("https://the-internet.herokuapp.com/");
+//
+//        //maximize window
+//        //driver.manage().window().maximize();
+//
+//        //full screen mode
+//        //driver.manage().window().fullscreen();
+//
+//        //set specific size
+//        //driver.manage().window().setSize(new Dimension(375, 812));
+//
+//
+//        //find several elements and count
+//        List<WebElement> links = driver.findElements(By.tagName("a"));
+//        System.out.println(links.size());
+//
+//        //find specific element by linktext
+//        //driver.findElement(By.linkText("Inputs"));
+//        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
+//        inputsLink.click();
+//
+//
+//
+//
+//
+//        System.out.println(driver.getTitle());
+//
+//        driver.quit();
+//    }
+//
+//    public static void main(String args[]){
+//        BaseTests test = new BaseTests();
+//        test.setUp();
+//    }
+//}
