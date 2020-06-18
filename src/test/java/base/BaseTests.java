ab5f37e07f28c9cbe11d394d441d8f49a1c94edf
wilson <wilsongumba@gmail.com>
2020-06-19 01:18:46 +0300

    Add Screenshot tests

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,10 +1,17 @@
 package base;
 
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
 import pages.HomePage;
 import utils.WindowManager;
+
+import java.io.File;
+import java.io.IOException;
 
 public class BaseTests {
 
@@ -31,6 +38,38 @@ public class BaseTests {
     public void tearDown(){
         driver.quit();
     }
+
+////take screenshot after every test
+//    @AfterMethod
+//    public void takeScreenshot(){
+//        var camera = (TakesScreenshot)driver;
+//        File screenshot = camera.getScreenshotAs(OutputType.FILE);
+//
+//        try {
+//            Files.move(screenshot,new File("resources/screenshots/test.png"));
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }
+//        System.out.println("Screenshot taken: " + screenshot.getAbsolutePath());
+//    }
+
+
+//take screenshot on fail
+    @AfterMethod
+    public void recordFailure(ITestResult result){
+        if (ITestResult.FAILURE == result.getStatus())
+        {
+            var camera = (TakesScreenshot)driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            try {
+                Files.move(screenshot,new File("resources/screenshots/" + result.getName() + ".png"));
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+
 
     public WindowManager getWindowManager(){
         return new WindowManager(driver);
